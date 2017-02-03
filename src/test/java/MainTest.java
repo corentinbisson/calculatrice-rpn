10a7fcc4ea3921be595b8c20f9a99a6419b9ec4c
corentinbisson <vitamine141@gmail.com>
2017-02-03 23:48:26 +0100

    ajout de tests, ajout de la fonction gamma d'Euler, meilleure gestion des exceptions

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,9 +10,28 @@ import org.junit.Test;
 public class MainTest extends TestCase {
     Main main = new Main();
     @Test
-    public void testCalculer() throws Exception {
+    public void testEvaluerOk() throws Exception {
         double d = 35;
         assertEquals(d,main.evaluer("3 4 sqrt + 10 3 - *"));
+    }
+
+    @Test
+    public void testEvaluerBadArgument() throws Exception {
+        assertEquals(Double.NaN,main.evaluer("bonjour sqrt camion 9 * + 5"));
+    }
+
+    @Test
+    public void testEvaluerBadCalcul() throws Exception {
+        assertEquals(Double.NaN,main.evaluer("3 + 5"));
+    }
+
+    @Test
+    public void testEvaluerCalculNonTermine() throws Exception {
+        assertEquals(Double.NaN,main.evaluer("3 4 sqrt"));
+    }
+
+    public void testEvaluerEulerNegatif() throws Exception {
+        assertEquals(Double.NaN,main.evaluer("-3 !"));
     }
 
 }
